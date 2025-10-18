50cec060af4b6a48140d7e156fcbd9107fb03b8d
Marcelo <marce.reparacion@gmail.com>
2025-10-18 01:44:05 -0300

    frmuser.cs
    se agrego conexión con BD el formulario User con todas sus funcionalidades

---
+++ Tp-1-Algoritmos V1.0.2/Usuario.cs
@@ -0,0 +1,10 @@
+public class Usuario
+{
+    public string Nombre { get; set; }
+    public string Apellido { get; set; }
+    public string Dni { get; set; }
+    public string Telefono { get; set; }
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public string Rol { get; set; }
+}
